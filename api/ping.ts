3a93053eeaf76eb0d9a94322eee5502e91f10a22
Hershell <skyckdqja@gmail.com>
2025-09-01 09:05:54 +0900

    fix: env name & tsconfig for Vercel ESM

--- api/ping.ts
+++ api/ping.ts
@@ -1,5 +1,5 @@
-export default async function handler(req: Request) {
-  return new Response("pong", { status: 200 });
+export const runtime = 'edge';
+
+export default function handler(req: Request) {
+  return new Response('pong', { status: 200 });
 }
-
-
